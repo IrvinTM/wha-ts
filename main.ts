ddde68c15e870dc0cd334e9a117b6626b496443c
IrvinTM <irvintm23@gmail.com>
2025-07-10 22:36:24 -0600

    added db connection

--- main.ts
+++ main.ts
@@ -1,32 +1,41 @@
-import {AuthenticationState, makeWASocket, useMultiFileAuthState}  from 'baileys'
-import P  from 'pino'
+import { AuthenticationState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, makeWASocket, useMultiFileAuthState } from 'baileys'
+import P from 'pino'
+import { useMySQLAuthState } from 'mysql-baileys'
+import 'dotenv/config'
 
 
-const usePostgressAuthState = ()=>{
-    return{
-        //TODO return a state object and a function that saves the creds to the database
-        state:
-    }
-    
-}
+async function connectToWhatsApp(sessionName: string) {
 
-async function connectToWhatsApp () {
-    const { state, saveCreds } = await usePostgressAuthState()
-    const sock = makeWASocket({
-        auth: state,
-        logger: P(),
+    const logger = P()
+
+    const { state, saveCreds, removeCreds } = await useMySQLAuthState({
+        session: sessionName, // required
+        password: process.env.DB_PASSWORD as string, // required
+        database: process.env.DB_NAME as string, // required
+        host: process.env.DB_HOST,
+        port: process.env.PORT ? parseInt(process.env.DB_PORT as string) : 3000,
+        user: process.env.DB_USER,
     })
-        
 
-    sock.ev.on('connection.update', (con)=>{
+    const { error, version } = await fetchLatestBaileysVersion()
+    if (error) {
+        console.log(`Session: ${sessionName} | No connection, check your internet.`)
+        return connectToWhatsApp(sessionName)
+    }
+
+    const sock = makeWASocket({
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, logger)
+        },
+        version: version
+    })
+
+    sock.ev.on('connection.update', (con) => {
         console.log(con)
     })
 
     sock.ev.on('creds.update', saveCreds)
 }
 
-
-
-
-
-connectToWhatsApp()
+connectToWhatsApp("session1")
